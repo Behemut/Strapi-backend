3e6e4a35358095e337995bc3cdf4a0de7614dcbb
Behemut <kevin_eliu@hotmail.es>
2021-04-01 21:15:47 -0600

    Añadiendo Provider Google

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://strapi-backend-dsm.herokuapp.com',
+ // url: 'https://strapi-backend-dsm.herokuapp.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'f460e3fd153d4a424c149dae6074a58e'),
